47fa6dba73de0876e283822c29cf377aa114366e
554961 <554961@escg.ac.uk>
2025-12-12 09:22:42 +0000

    db upload

---
+++ hotel_system.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2025 at 10:22 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotel_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+-- Error reading structure for table hotel_system.booking: #1932 - Table &#039;hotel_system.booking&#039; doesn&#039;t exist in engine
+-- Error reading data for table hotel_system.booking: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hotel_system`.`booking`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+-- Error reading structure for table hotel_system.customer: #1932 - Table &#039;hotel_system.customer&#039; doesn&#039;t exist in engine
+-- Error reading data for table hotel_system.customer: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hotel_system`.`customer`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotel`
+--
+-- Error reading structure for table hotel_system.hotel: #1932 - Table &#039;hotel_system.hotel&#039; doesn&#039;t exist in engine
+-- Error reading data for table hotel_system.hotel: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hotel_system`.`hotel`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+-- Error reading structure for table hotel_system.room: #1932 - Table &#039;hotel_system.room&#039; doesn&#039;t exist in engine
+-- Error reading data for table hotel_system.room: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hotel_system`.`room`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+-- Error reading structure for table hotel_system.staff: #1932 - Table &#039;hotel_system.staff&#039; doesn&#039;t exist in engine
+-- Error reading data for table hotel_system.staff: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hotel_system`.`staff`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
